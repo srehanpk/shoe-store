7605a2518ad1857a584ef781c0f35e2023ac779c
srahmed <srehanpk@gmail.com>
2020-07-15 14:16:11 +0500

    update css

---
+++ src/Notfound.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import './App.css';
+
+
+
+
+
+
+
+
+function NotFound() {
+  
+   
+  
+
+
+  return (
+    
+
+    <div className="heading" >
+    
+    
+    
+    <h1 className="title">Sorry<br/>Page Not Found! </h1>
+  
+    
+  
+
+    </div>
+  );
+}
+
+export default NotFound;
+
+
+
+      
+      
